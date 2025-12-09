67cb0968cad264ee9347522aaa177e85eb22ed3e
Niklas <nzt2009@icloud.com>
2025-12-09 07:32:33 -0600

    first push for nik

--- ContentView.swift
+++ ContentView.swift
@@ -6,7 +6,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, Kane!")
+            Text("Kane is the best!")
         }
     }
 }
